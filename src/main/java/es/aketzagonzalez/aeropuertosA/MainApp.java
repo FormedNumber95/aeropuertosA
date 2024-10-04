71e7af95f99f8ffee6ced37bdb62e4342b740209
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-10-04 10:50:37 +0200

    Acabado el ejercicio
    Añadida la funcionalidad a los botones y con eso acabado el ejercicio
    04/10/2024

--- src/main/java/es/aketzagonzalez/aeropuertosA/MainApp.java
+++ src/main/java/es/aketzagonzalez/aeropuertosA/MainApp.java
@@ -20,21 +20,47 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Clase MainApp.
+ */
 public class MainApp extends Application {
+    
+    /** El stage. */
     private static Stage stage;
 
+    /**
+     * Metodo Start que fija el tamaño minimo de la ventana y lanza el metodo 
+     * setRoot.
+     *
+     * @param s El stage
+     * @throws IOException Si hay algun errore de entrada/salida.
+     */
     @Override
-    public void start(@SuppressWarnings("exports") Stage s) throws IOException {
+    public void start(@SuppressWarnings("exports") Stage s) throws IOException{
         stage=s;
         stage.setMinHeight(453);
         stage.setMinWidth(600);
-        setRoot("aeropuertoA","");
+        setRoot("aeropuertoA","Encuesta");
     }
 
+    /**
+     * Llama al metodo setRoot de 2 parametros fijando el titulo como 
+     * el titulo del stage.
+     *
+     * @param fxml El nuevo root
+     * @throws IOException Si hay algun errore de entrada/salida.
+     */
     static void setRoot(String fxml) throws IOException {
         setRoot(fxml,stage.getTitle());
     }
 
+    /**
+     * Fija el titulo y el xml que se van a usar.
+     *
+     * @param fxml El fxml
+     * @param title El titulo
+     * @throws IOException Si hay algun errore de entrada/salida.
+     */
     static void setRoot(String fxml, String title) throws IOException {
         Scene scene = new Scene(loadFXML(fxml));
         stage.setTitle(title);
@@ -42,14 +68,36 @@ public class MainApp extends Application {
         stage.show();
     }
 
+    /**
+     * Crea un loader de FXML y asigna a este el fxml que recive.
+     *
+     * @param fxml El fxml
+     * @return El fxmlLoader
+     * @throws IOException Si hay algun errore de entrada/salida.
+     */
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/"+fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource
+        		("/fxml/"+fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
 
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
+    
+    /**
+     * Gets the stage.
+     *
+     * @return the stage
+     */
+    public static Stage getStage() {
+		return stage;
+	}
 
 }
--- src/main/java/es/aketzagonzalez/aeropuertosA/aeropuertoAController.java
+++ src/main/java/es/aketzagonzalez/aeropuertosA/aeropuertoAController.java
@@ -1,94 +1,176 @@
 package es.aketzagonzalez.aeropuertosA;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
+/**
+ * Clase aeropuertoAController.
+ */
 public class aeropuertoAController {
 
+    /** El btn aceptar. */
     @FXML
     private Button btnAceptar;
 
+    /** El btn cancelar. */
     @FXML
     private Button btnCancelar;
 
+    /** El radbtn hombre. */
     @FXML
     private RadioButton radBtnHombre;
 
+    /** El radbtn mujer. */
     @FXML
     private RadioButton radBtnMujer;
 
+    /** El radbtn otro. */
     @FXML
     private RadioButton radBtnOtro;
 
+    /** El chk deporte. */
     @FXML
     private CheckBox chkDeporte;
 
+    /** El cmb edad. */
     @FXML
     private ComboBox<String> cmbEdad;
 
+    /** El grupo sexo. */
     @FXML
     private ToggleGroup grupoSexo;
 
+    /** El lst deportes. */
     @FXML
     private ListView<String> lstDeportes;
 
+    /** El slider cine. */
     @FXML
     private Slider sliderCine;
 
+    /** El slider compra. */
     @FXML
     private Slider sliderCompra;
 
+    /** El slider televison. */
     @FXML
     private Slider sliderTelevison;
 
+    /** El txt num hermanos. */
     @FXML
     private TextField txtNumHermanos;
 
+    /** El txt profesion. */
     @FXML
     private TextField txtProfesion;
 
+    /**
+     * Al pulsar el boton aceptar se evaluan todos los campos, y si es valido,
+     * muestra toda la informacion, y en caso de no serlo, muestra que errores 
+     * tiene.
+     *
+     * @param event El evento
+     */
     @FXML
     void accionAceptar(ActionEvent event) {
-
-    }
-    
-    @FXML
-    void accionCancelar(ActionEvent event) {
-    	txtNumHermanos.setText("");
-    	txtProfesion.setText("");
-    	chkDeporte.setSelected(false);
-    	sliderCine.setValue(5);
-    	sliderCompra.setValue(5);
-    	sliderTelevison.setValue(5);
-    	radBtnHombre.setSelected(false);
-    	radBtnMujer.setSelected(false);
-    	radBtnOtro.setSelected(false);
-    }
-
-    @FXML
-    void acionPracticarDeporte(ActionEvent event) {
-    	if(chkDeporte.isSelected()) {
-    		lstDeportes.setDisable(false);
+    	String errores="";
+    	if(txtProfesion.getText().equals("")) {
+    		errores+="Hay que rellenar el campo profesion\n";
+    	}
+    	if(txtNumHermanos.getText().equals("")) {
+    		errores+="Hay que rellenar el campo Nº hermanos\n";
+    	}else {
+	    	try{
+	    		Integer.parseInt(txtNumHermanos.getText());
+	    		if(Integer.parseInt(txtNumHermanos.getText())<0) {
+	    			throw new Exception();
+	    		}
+	    	}catch(NumberFormatException e){
+	    		errores+="El Nº de hermanos dever ser un numero entero\n";
+	    	} catch (Exception e) {
+				errores+="El Nº de hermanos no puede ser menor que 0\n";
+			}
+    	}
+    	if(cmbEdad.getSelectionModel().getSelectedIndex()==-1) {
+    		errores+="Tienes que marcar tu grupo de edad\n";
+    	}
+    	if(chkDeporte.isSelected()&&lstDeportes.getSelectionModel().
+    			getSelectedItems().size()==0) {
+    		errores+="Tienes que indicar los deportes que practicas";
+    	}
+    	Alert mensaje=new Alert(Alert.AlertType.INFORMATION);
+    	mensaje.setTitle("TUS DATOS");
+    	mensaje.setHeaderText(null);
+    	if(!errores.equals("")) {
+    		mensaje.setAlertType(Alert.AlertType.ERROR);
+    		mensaje.setContentText(errores);
     	}
     	else {
-    		lstDeportes.setDisable(true);
+    		String texto="Profesion: ";
+    		texto+=txtProfesion.getText()+"\nNº Hermanos: "+
+    		txtNumHermanos.getText()+"\nEdad: "+
+    				cmbEdad.getSelectionModel().getSelectedItem()+"\n";
+    		if(chkDeporte.isSelected()) {
+    			texto+="Deportes que practicas:\n";
+    			for(String deporte:lstDeportes.getSelectionModel().
+    					getSelectedItems()) {
+    				texto+=deporte+"\n";
+    			}
+    		}
+    		texto+="Grado de aficion a las comras: "+sliderCompra.getValue()+
+    				"\nGrado de aficion a ver la television: "+
+    				sliderTelevison.getValue()+"\n Grado de aficion a ir al"
+    						+ " cine: "+sliderCine.getValue();
+    		mensaje.setContentText(texto);
     	}
+		mensaje.showAndWait();
     }
     
+    /**
+     * Al pulsar el boton cancelar la encuesta se cierra.
+     *
+     * @param event El evento
+     */
+    @FXML
+    void accionCancelar(ActionEvent event) {
+    	MainApp.getStage().close();
+    }
+
+    /**
+     * Habilita o deshabilita la lista de deportes en funcion del estado del 
+     * checkbox de chkDeporte.
+     *
+     * @param event El evento
+     */
+    @FXML
+    void acionPracticarDeporte(ActionEvent event) {
+    	lstDeportes.setDisable(!chkDeporte.isSelected());
+    }
+    
+    /**
+     * Inicializar los valores de la lista y el combo y poner la lista para 
+     * seleccion multiple.
+     */
     @FXML
     void initialize() {
-    	lstDeportes.getItems().addAll("Tenis","Fútbol","Baloncesto","Natación","Ciclismo","Otro");
-    	cmbEdad.getItems().addAll("Menores de 18","Entre 18 y 30","Entre 31 y 50","Entre 51 y 70","Mayores de 70");
-    	//hacer que se pueda elegir mas de una opcion de la lista
+    	lstDeportes.getItems().addAll("Tenis","Fútbol","Baloncesto",
+    			"Natación","Ciclismo","Otro");
+    	cmbEdad.getItems().addAll("Menores de 18","Entre 18 y 30",
+    			"Entre 31 y 50","Entre 51 y 70","Mayores de 70");
+    	lstDeportes.getSelectionModel().setSelectionMode(
+    			SelectionMode.MULTIPLE);
     }
 
 }
